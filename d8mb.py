f5c4490be36406dc37b4a6118d379e091e29c5e3
skuralll <f20154@e.hiroshima-cu.ac.jp>
2022-05-27 20:30:24 +0900

    GUI

---
+++ d8mb.py
@@ -0,0 +1,61 @@
+import keyboard
+from win32gui import GetWindowText, GetForegroundWindow
+import win32clipboard
+from PIL import ImageGrab, Image
+from io import BytesIO
+
+
+IMAGE_SIZE_MAX = 8000000
+
+
+def is_in_discord():
+    # todo  other os
+    window_name = GetWindowText(GetForegroundWindow())
+    app_name = window_name.split()[-1]
+    return app_name == "Discord"
+
+
+def is_clip_image():
+    return isinstance(ImageGrab.grabclipboard(), Image.Image)
+
+
+def copy_image_to_clipboard(data):
+    # todo  other os
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(win32clipboard.CF_DIB, data)
+    win32clipboard.CloseClipboard()
+
+
+def resize_image_clip():
+    image = ImageGrab.grabclipboard()
+    image_io = BytesIO()
+    resize_level = 1
+    while True:
+        image = image.resize((image.width // resize_level, image.height // resize_level))
+        image.save(image_io, "BMP")
+        if image_io.getbuffer().nbytes < IMAGE_SIZE_MAX:
+            break
+        resize_level += 1
+        image_io.seek(0)
+        image_io.truncate(0)
+    copy_image_to_clipboard(image_io.getvalue()[14:])
+    image_io.close()
+
+
+def on_paste():
+    try:
+        if is_in_discord() and is_clip_image():
+            resize_image_clip()
+        keyboard.press("ctrl+v")
+    except OSError as e:
+        print(e)
+    keyboard.release("ctrl+v")
+
+
+def begin_hook():
+    try:
+        keyboard.remove_hotkey("ctrl+v")
+    except KeyError:
+        pass
+    keyboard.add_hotkey("ctrl+v", on_paste, (), True)
---
+++ d8mb_gui.py
@@ -0,0 +1,32 @@
+from pystray import Icon, MenuItem, Menu
+from PIL import Image
+import sys
+import os
+import d8mb
+
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+
+class TaskTray:
+
+    def __init__(self):
+        self.status = False
+        menu = Menu(
+                    MenuItem('Rebind', self.hook),
+                    MenuItem('Exit', self.stop),
+                )
+        self.icon = Icon(name='Discord_8MB', title='Discord_8MB', icon=Image.open(resource_path("icon.ico")), menu=menu)
+        self.hook()
+
+    def run(self):
+        self.icon.run()
+
+    def stop(self):
+        self.icon.stop()
+
+    def hook(self):
+        d8mb.begin_hook()
