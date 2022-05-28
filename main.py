a11b5f3234e2c27bb59f1b71428c3d02469e66f3
skuralll <f20154@e.hiroshima-cu.ac.jp>
2022-05-28 23:21:28 +0900

    bug fix

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from d8mb_gui import run_gui
 from keyboard_handler import run_handler
+import time
 
 if __name__ == '__main__':
     gui_thread = run_gui()
     handler_thread = run_handler()
     while gui_thread.is_alive() and handler_thread.is_alive():
-        pass
+        time.sleep(1)
 
